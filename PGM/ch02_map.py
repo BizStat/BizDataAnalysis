4e95e5d257852767dd97f5b443692a5de07c6eb1
Dong-Hee Lee <dhl@kgu.ac.kr>
2024-03-18 08:26:33 +0900

    update

--- PGM/ch02_map.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
-
-# Read the shapefile
-sf = gpd.read_file('../DATA/TL_SCCO_SIG.shp')
-
-sf.head()
-sf.convex_hull.plot(color='gray', edgecolor="w")
